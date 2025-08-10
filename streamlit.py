8f3a8d3831b65ad0002441544f8d7cdad81b80a7
nileshkr11 <nileshkumarstudy@gmail.com>
2025-08-10 23:05:55 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,31 +1,25 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
+import gdown
+import os
 
-movies_dict=pickle.load(open('movie_dict. pkl', 'rb'))
-similarity=pickle.load(open('similarity.pkl', 'rb'))
-movies=pd.DataFrame(movies_dict)
+# Download similarity.pkl from Google Drive if not already present
+url = 'https://drive.google.com/uc?id=1dAkM4GRJRn8fhJTv1xoLMHEaap_zO8tg'
+output = 'similarity.pkl'
+if not os.path.exists(output):
+    gdown.download(url, output, quiet=False)
 
-# def fetch_poster(movie_id):
-#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-#     data = requests.get(url)
-#     data = data.json()
-#     poster_path = data['poster_path']
-#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-#     return full_path
+# Load other local pickle file
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
-def recommend (movie):
-    movie_index = movies[movies['title'] == movie].index [0]
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted (list (enumerate (distances)), reverse=True, key=lambda x:x[1])[2:7]
-    recommended_movies = []
-    recommended_movies_posters = []
-    for i in movies_list:
-        movie_id = i[0]
-        recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
-        # recommended_movies_posters.append(fetch_poster(movie_id))
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[2:7]
+    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]
     return recommended_movies
 
 st.title('Movie Recommender')
@@ -33,7 +27,8 @@ st.title('Movie Recommender')
 selected_movie_name = st.selectbox(
     'Select a movie you like:',
     movies['title'].values)
+
 if st.button('Recommend'):
-    recomendations=recommend(selected_movie_name)
-    for i in recomendations:
-        st.write(i)
+    recommendations = recommend(selected_movie_name)
+    for rec in recommendations:
+        st.write(rec)
